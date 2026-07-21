f290156b32c32b71b7984080d76b0051a286d181
Mustapha Haadi <mustaphahaadi04@gmail.com>
2026-07-21 20:25:29 +0000

    style: reformat HTML markup and apply consistent indentation across frontend files.

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,27 +1,20 @@
 (function ($) {
     "use strict";
 
-    // Guaranteed Spinner Removal
-    var hideSpinner = function () {
+    // Spinner
+    var spinner = function () {
         setTimeout(function () {
-            var $spinner = $('#spinner');
-            if ($spinner.length > 0) {
-                $spinner.removeClass('show');
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
             }
-        }, 100);
+        }, 1);
     };
+    spinner(0);
 
-    hideSpinner();
-    $(window).on('load', hideSpinner);
 
-    // Safe WOW.js initialization
-    if (typeof WOW !== 'undefined') {
-        try {
-            new WOW().init();
-        } catch (e) {
-            console.warn('[AlphaPass] WOW.js init skipped:', e.message);
-        }
-    }
+    // Initiate the wowjs
+    new WOW().init();
+
 
     // Sticky Navbar
     $(window).scroll(function () {
@@ -32,47 +25,139 @@
         }
     });
 
-    // Hero Header Carousel
-    if ($.fn.owlCarousel && $(".header-carousel").length > 0) {
-        $(".header-carousel").owlCarousel({
-            items: 1,
-            autoplay: true,
-            smartSpeed: 2000,
-            center: false,
-            dots: false,
-            loop: true,
-            margin: 0,
-            nav: true,
-            navText: [
-                '<i class="bi bi-arrow-left"></i>',
-                '<i class="bi bi-arrow-right"></i>'
-            ]
-        });
-    }
 
-    // Product List Carousel
-    if ($.fn.owlCarousel && $(".productList-carousel").length > 0) {
-        $(".productList-carousel").owlCarousel({
-            autoplay: true,
-            smartSpeed: 2000,
-            dots: false,
-            loop: true,
-            margin: 25,
-            nav: true,
-            navText: [
-                '<i class="fas fa-chevron-left"></i>',
-                '<i class="fas fa-chevron-right"></i>'
-            ],
-            responsiveClass: true,
-            responsive: {
-                0: { items: 1 },
-                576: { items: 1 },
-                768: { items: 2 },
-                992: { items: 2 },
-                1200: { items: 3 }
+    // Hero Header carousel
+    $(".header-carousel").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 2000,
+        center: false,
+        dots: false,
+        loop: true,
+        margin: 0,
+        nav: true,
+        navText: [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ]
+    });
+
+
+    // ProductList carousel
+    $(".productList-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 2000,
+        dots: false,
+        loop: true,
+        margin: 25,
+        nav: true,
+        navText: [
+            '<i class="fas fa-chevron-left"></i>',
+            '<i class="fas fa-chevron-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 2
+            },
+            1200: {
+                items: 3
             }
-        });
-    }
+        }
+    });
+
+    // ProductList categories carousel
+    $(".productImg-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        dots: false,
+        loop: true,
+        items: 1,
+        margin: 25,
+        nav: true,
+        navText: [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ]
+    });
+
+
+    // Single Products carousel
+    $(".single-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        dots: true,
+        dotsData: true,
+        loop: true,
+        items: 1,
+        nav: true,
+        navText: [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ]
+    });
+
+
+    // ProductList carousel
+    $(".related-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        dots: false,
+        loop: true,
+        margin: 25,
+        nav: true,
+        navText: [
+            '<i class="fas fa-chevron-left"></i>',
+            '<i class="fas fa-chevron-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1200: {
+                items: 4
+            }
+        }
+    });
+
+
+
+    // Product Quantity
+    $('.quantity button').on('click', function () {
+        var button = $(this);
+        var oldValue = button.parent().parent().find('input').val();
+        if (button.hasClass('btn-plus')) {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            if (oldValue > 0) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 0;
+            }
+        }
+        button.parent().parent().find('input').val(newVal);
+    });
+
+
 
     // Back to top button
     $(window).scroll(function () {
@@ -87,4 +172,8 @@
         return false;
     });
 
+
+
+
 })(jQuery);
+
