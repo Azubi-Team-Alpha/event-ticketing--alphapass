a2e717f4638821fbf9f82e85041ea16085456781
Mustapha Haadi <mustaphahaadi04@gmail.com>
2026-07-22 10:33:23 +0000

    refactor: restrict S3 CORS allowed methods to GET and HEAD

--- infra/modules/s3/main.tf
+++ infra/modules/s3/main.tf
@@ -60,7 +60,7 @@ resource "aws_s3_bucket_cors_configuration" "frontend" {
 
   cors_rule {
     allowed_headers = ["*"]
-    allowed_methods = ["GET", "HEAD", "OPTIONS"]
+    allowed_methods = ["GET", "HEAD"]
     allowed_origins = var.cors_allowed_origins
     expose_headers  = ["ETag"]
     max_age_seconds = 3600
